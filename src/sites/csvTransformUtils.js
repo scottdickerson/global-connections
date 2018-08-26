8db2cc4eebaf9dcb734e138039b12081dbf62461
swiftfoot <swiftfoot@hotmail.com>
2018-08-26 16:15:47 -0500

    feat(DetailImages): Added detail images

--- src/sites/csvTransformUtils.js
+++ src/sites/csvTransformUtils.js
@@ -1,9 +1,61 @@
 import csvtojson from "csvtojson";
 import path from "path";
+import find from "find";
 
-const loadGlobalConnections = () =>
+function escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
+export const findPhotoFile = (photoId, photoDir = "details-images") => {
+  const fileRegExp = new RegExp(
+    `\\\\${escapeRegExp(photoId)}.*\\.(?:png|jpg)$`,
+    "g"
+  );
+  const fileArray = find.fileSync(
+    fileRegExp,
+    path.join(__dirname, "..", "..", "public", "img", photoDir)
+  );
+  // return just the short name of the file
+  return fileArray.length > 0 ? path.basename(fileArray.pop()) : null;
+};
+
+export const loadGlobalConnections = () =>
   csvtojson()
     .fromFile(path.join(__dirname, "/GlobalConnectionsText.csv"))
-    .then(jsonObj => jsonObj);
-
-export default loadGlobalConnections;
+    .then(jsonObj => {
+      const mappedObject = jsonObj
+        .filter(globalSite => !globalSite.Continent) // exclude any row that's just a continent
+        .map(globalSite => ({
+          person: globalSite["Person/ Group/ Event Name"],
+          label: globalSite["Star Point"],
+          // top: translate location from the label
+          // left: translate location from the label
+          photoId: globalSite["Photo(s)"],
+          // thumbnail: findThumbnail(photoId),
+          detailImages: [
+            {
+              src: findPhotoFile(globalSite["Photo(s)"].split(" ").shift()), // replace with filename finder
+              title: globalSite.Conflict,
+              caption: globalSite.Caption,
+              credit: globalSite["Photo Credit"]
+            }
+          ],
+          learnMore: globalSite["Long Story (Opt?)"]
+        }));
+      const filteredSites = mappedObject.filter(
+        globalSite => globalSite.photoId
+      );
+      const nestedSite = filteredSites.reduce((result, globalSite) => {
+        // console.log(`nestedSite: ${JSON.stringify(globalSite)}`);
+        if (!globalSite.person) {
+          result[result.length - 1].detailImages.push(
+            ...globalSite.detailImages
+          );
+        } else {
+          result.push(globalSite);
+        }
+        return result;
+      }, []);
+      // console.log(JSON.stringify(nestedSite, null, "\t"));
+      return nestedSite;
+    });
--- src/sites/csvTransformUtils.test.js
+++ src/sites/csvTransformUtils.test.js
@@ -1,8 +1,24 @@
-import loadGlobalConnections from "./csvTransformUtils";
+import _ from "lodash";
+import { loadGlobalConnections, findPhotoFile } from "./csvTransformUtils";
 
 describe(`csvTransformUtils`, () => {
-  test("loadGlobalConnections", () =>
-    loadGlobalConnections().then(jsonObject =>
-      expect(jsonObject).toBeDefined()
-    ));
+  test("loadGlobalConnections", done =>
+    loadGlobalConnections().then(jsonObject => {
+      expect(jsonObject).toBeDefined();
+      const missingSites = jsonObject.map(site => ({
+        person: `${site.photoId} ${site.person}`,
+        missingImageSrc: _.some(site.detailImages, { src: null })
+      }));
+      console.log(
+        `missingSites: ${JSON.stringify(
+          missingSites.filter(site => site.missingImageSrc),
+          null,
+          "\t"
+        )}`
+      );
+      done();
+    }));
+  test("findPhotoFile", () => {
+    expect(findPhotoFile("4.0")).toBeDefined();
+  });
 });
